aee99dc7f41983bd9866f25534c8a13f644419ba
Danil <danil_ustinov@mail.ru>
2024-11-25 14:02:04 +0700

    remove IDeepPartial from tables

--- backend/src/entrant/infrastructure/tables/EntrantTable.ts
+++ backend/src/entrant/infrastructure/tables/EntrantTable.ts
@@ -1,6 +1,5 @@
-import {IDeepPartial} from '@steroidsjs/nest/src/usecases/interfaces/IDeepPartial';
 import {TableFromModel} from '@steroidsjs/nest/src/infrastructure/decorators/TableFromModel';
 import {EntrantModel} from '../../domain/models/EntrantModel';
 
 @TableFromModel(EntrantModel, 'entrant')
-export class EntrantTable implements IDeepPartial<EntrantModel> {}
+export class EntrantTable extends EntrantModel {}
--- backend/src/test/infrastructure/tables/TestTable.ts
+++ backend/src/test/infrastructure/tables/TestTable.ts
@@ -1,6 +1,5 @@
-import {IDeepPartial} from '@steroidsjs/nest/src/usecases/interfaces/IDeepPartial';
 import {TableFromModel} from '@steroidsjs/nest/src/infrastructure/decorators/TableFromModel';
 import {TestModel} from '../../domain/models/TestModel';
 
 @TableFromModel(TestModel, 'Test_test')
-export class TestTable implements IDeepPartial<TestModel> {}
+export class TestTable extends TestModel {}
